a9dd241151a0dae5413d844a66da7d08f3c3b9f0
Devaud <devaud.alan@gmail.com>
2014-11-06 16:09:05 +0100

    Story n°1 : Création de la classe manipdb.cs pour la gestion de la base de données. Il y a seulement la fonction de connexion, pour l'instant.

--- Cabane/Cabane/FrmCabane.cs
+++ Cabane/Cabane/FrmCabane.cs
@@ -12,9 +12,18 @@ namespace Cabane
 {
     public partial class FrmCabane : Form
     {
+       
+
         public FrmCabane()
         {
             InitializeComponent();
+        }
+
+        private void FrmCabane_Load(object sender, EventArgs e)
+        {
+            manipdb db = new manipdb(); 
+
+            label1.Text = db.connexion("127.0.0.1", "root", "myblogdb");
         }
     }
 }
---
+++ Cabane/Cabane/manipdb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Cabane
+{
+    class manipdb
+    {
+        private MySqlConnection conn; //Objet de connexion a la db
+        private MySqlDataReader rdr; // Objet pour lire les informations retournée par une requete
+        private MySqlCommand cmd; // Objet qui permet de faire une requete
+        private String nomConnexion;
+
+        public manipdb()
+        {
+            conn = new MySqlConnection();
+            rdr = null;
+        }
+
+        public string connexion(string serveur, string uid, string database, string pwd="")
+        {
+            try
+            {
+                nomConnexion = "server="+serveur+";uid="+uid+";pwd="+pwd+";database="+database+";";
+
+                conn.ConnectionString = nomConnexion;
+                conn.Open();
+                return "Ok";
+            }
+            catch(MySqlException ex)
+            {
+                return ex.Message;
+            }
+
+        }
+    }
+}
