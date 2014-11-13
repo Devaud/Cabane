b905bf8ba9735630ebbf399824b46fe99a4d990a
Bruninho-203 <santos.bruno1203@gmail.com>
2014-11-13 12:58:22 +0100

    Story n° 4 : AJout d'une fiche pour la liste des cabanes

---
+++ Cabane/Cabane/FrmListeCabane.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Cabane
+{
+    partial class FrmListeCabane
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxCabanes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbxCabanes
+            // 
+            this.lbxCabanes.FormattingEnabled = true;
+            this.lbxCabanes.Items.AddRange(new object[] {
+            "aaaa",
+            "bbb",
+            "ccc",
+            "ddd",
+            "eee"});
+            this.lbxCabanes.Location = new System.Drawing.Point(1, 0);
+            this.lbxCabanes.Name = "lbxCabanes";
+            this.lbxCabanes.Size = new System.Drawing.Size(283, 264);
+            this.lbxCabanes.TabIndex = 0;
+            this.lbxCabanes.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lbxCabanes);
+            this.Name = "Form1";
+            this.Text = "Liste des cabanes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxCabanes;
+    }
+}
---
+++ Cabane/Cabane/FrmListeCabane.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cabane
+{
+    public partial class FrmListeCabane : Form
+    {
+        public FrmListeCabane()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
